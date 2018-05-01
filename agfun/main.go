0deb414b0546ec2ca61a58653579fe0252c5685e
FengchenX <fengchenxiaonvsheng@hotmail.com>
2018-05-01 12:46:34 +0800

    add

--- agfun/main.go
+++ agfun/main.go
@@ -34,7 +34,7 @@ func main() {
 /**网站首页*/
 func agfun(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("visit first index")	
-	t, _ := template.ParseFiles("./view/index.html")
+	t, _ := template.ParseFiles("./view/templates/index.html")
 	t.Execute(w, nil)
 }
 
