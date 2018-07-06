f5b2791e3d667d0dad97ea5cd0b0d2bd42d3675f
unknown <chenfeng@diag.launch>
2018-07-06 14:32:06 +0800

    add

---
+++ design/builder/builder/builder.go
@@ -0,0 +1,119 @@
+package builder
+
+const (
+	FOOD  = "food"
+	DRINK = "drink"
+)
+
+type Item interface {
+	Price() float32
+	Name() string
+	Category() string
+}
+
+type Food struct {
+}
+
+func (Food) Price() float32 {
+	return 0.0
+}
+
+func (Food) Name() string {
+	return ""
+}
+
+func (Food) Category() string {
+	return FOOD
+}
+
+type Drink struct {
+}
+
+func (Drink) Price() float32 {
+	return 0.0
+}
+
+func (Drink) Name() string {
+	return ""
+}
+
+func (Drink) Category() string {
+	return DRINK
+}
+
+type Hamburger struct {
+	Food
+}
+
+func (Hamburger) Price() float32 {
+	return 12.00
+}
+
+func (Hamburger) Name() string {
+	return "Hamburger"
+}
+
+type FriedChicken struct {
+	Food
+}
+
+func (FriedChicken) Price() float32 {
+	return 18.00
+}
+
+func (FriedChicken) Name() string {
+	return "FriedChicken"
+}
+
+type Cola struct {
+	Drink
+}
+
+func (Cola) Price() float32 {
+	return 3.00
+}
+
+func (Cola) Name() string {
+	return "Cola"
+}
+
+type Beer struct {
+	Drink
+}
+
+func (Beer) Price() float32 {
+	return 5.00
+}
+
+func (Beer) Name() string {
+	return "Beer"
+}
+
+type Meal []Item
+
+func (m *Meal) AddItem(item ...Item) {
+	*m = append(*m, item...)
+}
+
+func (m Meal) Cost() (cost float32) {
+	for _, val := range m {
+		cost += val.Price()
+	}
+	return
+}
+
+func (m Meal) ShowItems() (msg string) {
+	for _, val := range m {
+		msg += "Category: " + val.Category() + "Name: " + val.Name() + "\n"
+	}
+	return
+}
+
+type MealBuilder struct {
+}
+
+func (MealBuilder) MealOne() (meal *Meal) {
+	meal = new(Meal)
+	meal.AddItem(new(FriedChicken), new(Beer))
+	return
+}
--- design/builder/main.go
+++ design/builder/main.go
@@ -1,16 +1,5 @@
 package main
 
-
-import (
-	"github.com/feng/future/design/builder/car"
-)
-
 func main() {
-	assembly := car.NewBuilder().Paint(car.RedColor)
 
-	familyCar := assembly.Wheels(car.SportsWheels).TopSpeed(50 * car.MPH).Build()
-	familyCar.Drive()
-
-	sportsCar := assembly.Wheels(car.SteelWheels).TopSpeed(150 * car.MPH).Build()
-	sportsCar.Drive()
 }
