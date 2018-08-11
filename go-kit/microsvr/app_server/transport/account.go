b1563d4d2f2a0f1d2ea4f9dcdd568f56bf9c7567
FengchenX <fengchenxiaonvsheng@hotmail.com>
2018-08-11 10:41:35 +0800

    add

--- go-kit/microsvr/app_server/transport/account.go
+++ go-kit/microsvr/app_server/transport/account.go
@@ -7,8 +7,6 @@ import (
 	"github.com/feng/future/go-kit/microsvr/app_server/model"
 	"net/http"
 	"encoding/json"
-	"bytes"
-	"io/ioutil"
 )
 
 func makeGetAccountEndpoint(svc service.AppService) endpoint.Endpoint {
@@ -29,19 +27,6 @@ func decodeGetAccountRequest(_ context.Context, r *http.Request) (interface{}, e
 		return nil, err
 	}
 	return request, nil
-}
-
-func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
-	return json.NewEncoder(w).Encode(response)
-}
-
-func encodeRequest(_ context.Context, r *http.Request, request interface{}) error {
-	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(request); err != nil {
-		return err
-	}
-	r.Body = ioutil.NopCloser(&buf)
-	return nil
 }
 
 //ReqGetAccount 客户端 查询支付账号
--- go-kit/microsvr/app_server/transport/transport.go
+++ go-kit/microsvr/app_server/transport/transport.go
@@ -43,3 +43,16 @@ func encodeError(_ context.Context, err error, w http.ResponseWriter) {
 		"error": err.Error(),
 	})
 }
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
+func encodeRequest(_ context.Context, r *http.Request, request interface{}) error {
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(request); err != nil {
+		return err
+	}
+	r.Body = ioutil.NopCloser(&buf)
+	return nil
+}
