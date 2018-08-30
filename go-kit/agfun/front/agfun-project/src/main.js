70ab18fd572f1b4f7f3010b63bcaf8e9f1fab5e3
unknown <chenfeng@diag.launch>
2018-08-30 20:05:07 +0800

    add

---
+++ go-kit/agfun/front/agfun-project/src/main.js
@@ -0,0 +1,11 @@
+
+var exampleData = {
+    message: 'Hello World'
+}
+
+// 创建一个 Vue 实例或 "ViewModel"
+// 它连接 View 与 Model
+new Vue({
+    el: '#app',
+    data: exampleData
+})
--- go-kit/agfun/front/main.js
+++
@@ -1,9 +0,0 @@
-var vm = new Vue({
-    el: '#app',
-    data: {
-        yes: true,
-        no: false,
-        age: 28,
-        name: 'keepfool'
-    }
-})
