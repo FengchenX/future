2ac30d06339d38d8c1379492a609308dd9edc310
unknown <chenfeng@diag.launch>
2018-09-26 13:49:58 +0800

    add

--- go-kit/agfun/front/vue-project/src/views/Register.vue
+++ go-kit/agfun/front/vue-project/src/views/Register.vue
@@ -1,7 +1,21 @@
 <template>
-    <div >
+    <div class="register">
         <!-- <MyVue msg="Welcome to Your Vue.js App"/> -->
-        注册
+        <div class="center">
+            <div id="userName">UserName:
+                <input v-model="userName">
+            </div>
+            <div class="password1">Password:
+                <input v-model="password1">
+            </div>
+            <div class="password2">Password:
+                <input v-model="password2">
+            </div>
+            <div id="ensure">
+                <button v-on:click="register">register</button>
+                <button v-on:click="login">login</button>
+            </div>
+        </div>
     </div>
 </template>
  
@@ -12,10 +26,38 @@ export default {
     name: 'register',
     components: {
         // MyVue
+    },
+    data() {
+        return {
+            userName: '',
+            password1: '',
+            password2: ''
+        }
+    },
+    methods: {
+        register: function(){
+            console.log('register', this.userName, this.password1, this.password2, this);
+            this.$router.push({path:'/view1'});
+        },
+        login: function(){
+            console.log('login')
+        }
     }
 }
 </script>
  
-<style>
+<style scoped>
+.register{
+    height: 50rem;
     
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.center{
+    width: 40rem;
+    height: 25rem;
+    /* background-color: yellow; */
+
+}
 </style>
--- go-kit/agfun/front/vue-project/src/views/view1.vue
+++ go-kit/agfun/front/vue-project/src/views/view1.vue
@@ -1,6 +1,6 @@
 <template>
     <div >
-        <MyVue msg="Welcome to Your Vue.js App"/>
+        <!-- <MyVue msg="Welcome to Your Vue.js App"/> -->
     </div>
 </template>
  
