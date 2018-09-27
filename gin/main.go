76e946eb85cc73c9278758150b558e8f5a008215
unknown <chenfeng@diag.launch>
2018-09-27 14:57:53 +0800

    add

--- gin/main.go
+++ gin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -9,10 +10,16 @@ func main() {
 	router := gin.Default()
 
     router.GET("/", func(c *gin.Context) {
+		query := c.Request.URL.Query()
+		fmt.Println(query.Get("args1"))
 		c.String(http.StatusOK, "Hello World")
 	})
 	
 	router.POST("/test", func(c *gin.Context){
+		var req struct{}
+		if err := c.BindJSON(&req); err != nil {
+			panic(err)
+		}
 		c.String(http.StatusOK, "test")
 	})
     router.Run(":8000")
